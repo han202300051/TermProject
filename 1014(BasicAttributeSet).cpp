5b1b87490f5d79ebf5bf90a848ee8f16ab243b5b
Han <jhaaanul10@gmail.com>
2025-10-21 01:44:30 +0900

    Create 1014(BasicAttributeSet).cpp

---
+++ 1014(BasicAttributeSet).cpp
@@ -0,0 +1,57 @@
+#include "BasicAttributeSet.h"
+#include "Net/UnrealNetwork.h"   // 속성 복제를 위해 필요
+
+// 기본 생성자 - 초기 능력치 설정
+UBasicAttributeSet::UBasicAttributeSet()
+{
+	Health = 100.f;
+	MaxHealth = 100.f;
+	Stamina = 100.f;
+	MaxStamina = 100.f;
+}
+
+// 복제될 속성 등록
+void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 서버 → 클라이언트로 속성 값 복제 설정
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
+
+// 속성 값이 바뀌기 전에 호출됨
+void UBasicAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	// 체력 값이 MaxHealth보다 크지 않도록 제한
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+	// 스태미나 값이 MaxStamina보다 크지 않도록 제한
+	else if (Attribute == GetMaxStaminaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxStamina());
+	}
+}
+
+// GameplayEffect가 적용된 후 호출됨
+void UBasicAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	// 체력이 변화했을 때
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+	}
+	// 스태미나가 변화했을 때
+	else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+	{
+		SetStamina(FMath::Clamp(GetStamina(), 0.0f, GetMaxStamina()));
+	}
+}
