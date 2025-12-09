39010200dc08e502bf0787bd2495547e4dbe3f38
Han <jhaaanul10@gmail.com>
2025-12-09 12:12:27 +0900

    Create Weapon Inventory.h

---
+++ Weapon Inventory.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WeaponBase.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class YOURGAME_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMyCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	/** 최대 무기 슬롯 4개 */
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	TArray<TSubclassOf<AWeaponBase>> DefaultWeapons;
+
+	/** 실제 스폰된 무기 오브젝트 */
+	UPROPERTY()
+	TArray<AWeaponBase*> WeaponSlots;
+
+	/** 현재 장착된 무기 */
+	int32 CurrentWeaponIndex = -1;
+
+public:
+	/** 무기 선택 */
+	void SelectWeapon(int32 Index);
+
+	/** 무기 추가 함수 (상자 루팅 등 적용 가능) */
+	void AddWeaponToSlot(TSubclassOf<AWeaponBase> NewWeapon, int32 SlotIndex);
+
+	/** 공격 함수 → 현재 무기 Fire 호출 */
+	void StartFire();
+};
